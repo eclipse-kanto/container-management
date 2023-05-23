afb8f35407eea867aad3d5243f5db96f7f466243
Dimitar Dimitrov <dimitar.dimitrov3@bosch.io>
2023-05-23 12:09:03 +0300

    [#161] Error while trying to create container on raspberry pi (#162)
    Default to fallback platform match when a multi-arch container image is authorized by imgcrypt library
    
    Signed-off-by: Dimitar Dimitrov <dimitar.dimitrov3@bosch.io>

--- containerm/ctr/ctrd_decrypt_mgr.go
+++ containerm/ctr/ctrd_decrypt_mgr.go
@@ -14,12 +14,19 @@ package ctr
 
 import (
 	"context"
+	"encoding/json"
+	"sort"
+
 	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/content"
+	"github.com/containerd/containerd/images"
+	"github.com/containerd/containerd/platforms"
 	"github.com/containerd/imgcrypt/images/encryption"
 	"github.com/containerd/imgcrypt/images/encryption/parsehelpers"
 	ocicryptconfig "github.com/containers/ocicrypt/config"
 	"github.com/eclipse-kanto/container-management/containerm/containers/types"
 	"github.com/eclipse-kanto/container-management/containerm/log"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
 type containerDecryptMgr interface {
@@ -67,5 +74,60 @@ func (mgr *ctrDecryptMgr) CheckAuthorization(ctx context.Context, image containe
 	if dc == nil {
 		dc = &ocicryptconfig.DecryptConfig{}
 	}
-	return encryption.CheckAuthorization(ctx, image.ContentStore(), image.Target(), dc)
+
+	desc, err := getPlatformSpecificManifest(ctx, image)
+	if err != nil {
+		log.ErrorErr(err, "could not get the platform specific manifest of image ID = %s", image.Name())
+		return err
+	}
+
+	return encryption.CheckAuthorization(ctx, image.ContentStore(), desc, dc)
+}
+
+// imgcrypt library does not handle properly the case when a multi-arch index descriptor is provided in some corner cases
+// e.g. 32bit Raspberry Pi OS running on cpu with 64bit architecture
+func getPlatformSpecificManifest(ctx context.Context, image containerd.Image) (ocispec.Descriptor, error) {
+	desc := image.Target()
+	if desc.MediaType != ocispec.MediaTypeImageIndex && desc.MediaType != images.MediaTypeDockerSchema2ManifestList {
+		// if not a multi-arch image, proceed as it is
+		return desc, nil
+	}
+
+	var index ocispec.Index
+	if blob, err := content.ReadBlob(ctx, image.ContentStore(), desc); err != nil {
+		return ocispec.Descriptor{}, err
+	} else if err = json.Unmarshal(blob, &index); err != nil {
+		return ocispec.Descriptor{}, err
+	}
+
+	manifests := index.Manifests
+	if len(manifests) == 0 {
+		return ocispec.Descriptor{}, log.NewErrorf("no manifests are found")
+	}
+
+	// strict platform match
+	platform := platforms.DefaultSpec()
+	for _, manifest := range manifests {
+		if platforms.NewMatcher(platform).Match(*manifest.Platform) {
+			return manifest, nil
+		}
+	}
+
+	// default to less strict platform match - e.g. fallback to lower arm variants
+	matcher := platforms.Default()
+	sort.SliceStable(manifests, func(i, j int) bool {
+		if manifests[i].Platform == nil {
+			return false
+		}
+		if manifests[j].Platform == nil {
+			return true
+		}
+		return matcher.Less(*manifests[i].Platform, *manifests[j].Platform)
+	})
+
+	if matcher.Match(*manifests[0].Platform) {
+		return manifests[0], nil
+	}
+
+	return ocispec.Descriptor{}, log.NewErrorf("no match for platform = %+v", platform)
 }
