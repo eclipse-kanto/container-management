2c53cfdcf82c3f6b7dddaf445e7cd9046220f917
Stoyan Zoubev <Stoyan.Zoubev@bosch.io>
2023-07-10 18:11:56 +0300

    Fix PR review comments: - add names to test cases with valid input - use t.Run to execute test cases

--- containerm/util/containers_parse_test.go
+++ containerm/util/containers_parse_test.go
@@ -26,11 +26,11 @@ type errorTest struct {
 }
 
 func TestParseDeviceMappings(t *testing.T) {
-	testCases := []struct {
+	testCases := map[string]struct {
 		inputString    string
 		expectedDevice *types.DeviceMapping
 	}{
-		{
+		"test_parse_device_mapping_valid_input_without_cgrops": {
 			inputString: "/dev/ttyACM0:/dev/ttyUSB0",
 			expectedDevice: &types.DeviceMapping{
 				PathOnHost:        "/dev/ttyACM0",
@@ -38,7 +38,7 @@ func TestParseDeviceMappings(t *testing.T) {
 				CgroupPermissions: "rwm",
 			},
 		},
-		{
+		"test_parse_device_mapping_valid_input_with_readonly": {
 			inputString: "/dev/ttyACM1:/dev/ttyUSB1:r",
 			expectedDevice: &types.DeviceMapping{
 				PathOnHost:        "/dev/ttyACM1",
@@ -46,7 +46,7 @@ func TestParseDeviceMappings(t *testing.T) {
 				CgroupPermissions: "r",
 			},
 		},
-		{
+		"test_parse_device_mapping_valid_input_with_two_cgroup_permissions": {
 			inputString: "/dev/ttyACM2:/dev/ttyUSB2:mw",
 			expectedDevice: &types.DeviceMapping{
 				PathOnHost:        "/dev/ttyACM2",
@@ -56,22 +56,29 @@ func TestParseDeviceMappings(t *testing.T) {
 		},
 	}
 
+	index := 0
 	inputStrings := make([]string, len(testCases))
 	expectedDevices := make([]types.DeviceMapping, len(testCases))
-	for index, testCase := range testCases {
-		t.Log(testCase.inputString)
 
-		res, err := ParseDeviceMapping(testCase.inputString)
-		testutil.AssertNil(t, err)
-		testutil.AssertEqual(t, testCase.expectedDevice, res)
+	for testName, testCase := range testCases {
+		t.Run(testName, func(t *testing.T) {
+			t.Log(testCase.inputString)
 
-		inputStrings[index] = testCase.inputString
-		expectedDevices[index] = *res
+			res, err := ParseDeviceMapping(testCase.inputString)
+			testutil.AssertNil(t, err)
+			testutil.AssertEqual(t, testCase.expectedDevice, res)
+
+			inputStrings[index] = testCase.inputString
+			expectedDevices[index] = *res
+			index++
+		})
 	}
 
-	res, err := ParseDeviceMappings(inputStrings)
-	testutil.AssertNil(t, err)
-	testutil.AssertEqual(t, expectedDevices, res)
+	t.Run("test_parse_device_mapping_multiple", func(t *testing.T) {
+		res, err := ParseDeviceMappings(inputStrings)
+		testutil.AssertNil(t, err)
+		testutil.AssertEqual(t, expectedDevices, res)
+	})
 }
 
 func TestParseDeviceMappingsError(t *testing.T) {
@@ -106,22 +113,24 @@ func TestParseDeviceMappingsError(t *testing.T) {
 	inputStrings[0] = "/dev/ttyACM0:/dev/ttyUSB0"
 
 	for testName, testCase := range testCases {
-		t.Log(testName)
+		t.Run(testName, func(t *testing.T) {
+			t.Log(testName)
 
-		inputStrings[1] = testCase.inputString
+			inputStrings[1] = testCase.inputString
 
-		res, err := ParseDeviceMappings(inputStrings)
-		testutil.AssertError(t, log.NewErrorf(testCase.errMessage+" %s", testCase.inputString), err)
-		testutil.AssertNil(t, res)
+			res, err := ParseDeviceMappings(inputStrings)
+			testutil.AssertError(t, log.NewErrorf(testCase.errMessage+" %s", testCase.inputString), err)
+			testutil.AssertNil(t, res)
+		})
 	}
 }
 
 func TestParseMountPoints(t *testing.T) {
-	testCases := []struct {
+	testCases := map[string]struct {
 		inputString   string
 		expectedMount *types.MountPoint
 	}{
-		{
+		"test_parse_mount_point_valid_input_propagation_mode_missing": {
 			inputString: "/home/someuser:/home/root",
 			expectedMount: &types.MountPoint{
 				Source:          "/home/someuser",
@@ -129,7 +138,7 @@ func TestParseMountPoints(t *testing.T) {
 				PropagationMode: types.RPrivatePropagationMode,
 			},
 		},
-		{
+		"test_parse_mount_point_valid_input_propagation_mode_private": {
 			inputString: "/home/someuser:/home/root:private",
 			expectedMount: &types.MountPoint{
 				Source:          "/home/someuser",
@@ -137,7 +146,7 @@ func TestParseMountPoints(t *testing.T) {
 				PropagationMode: types.PrivatePropagationMode,
 			},
 		},
-		{
+		"test_parse_mount_point_valid_input_propagation_mode_rprivate": {
 			inputString: "/var:/var:rprivate",
 			expectedMount: &types.MountPoint{
 				Source:          "/var",
@@ -145,7 +154,7 @@ func TestParseMountPoints(t *testing.T) {
 				PropagationMode: types.RPrivatePropagationMode,
 			},
 		},
-		{
+		"test_parse_mount_point_valid_input_propagation_mode_shared": {
 			inputString: "/etc:/etc:shared",
 			expectedMount: &types.MountPoint{
 				Source:          "/etc",
@@ -153,7 +162,7 @@ func TestParseMountPoints(t *testing.T) {
 				PropagationMode: types.SharedPropagationMode,
 			},
 		},
-		{
+		"test_parse_mount_point_valid_input_propagation_mode_rshared": {
 			inputString: "/usr/bin:/usr/bin:rshared",
 			expectedMount: &types.MountPoint{
 				Source:          "/usr/bin",
@@ -161,7 +170,7 @@ func TestParseMountPoints(t *testing.T) {
 				PropagationMode: types.RSharedPropagationMode,
 			},
 		},
-		{
+		"test_parse_mount_point_valid_input_propagation_mode_slave": {
 			inputString: "/data:/data:slave",
 			expectedMount: &types.MountPoint{
 				Source:          "/data",
@@ -169,7 +178,7 @@ func TestParseMountPoints(t *testing.T) {
 				PropagationMode: types.SlavePropagationMode,
 			},
 		},
-		{
+		"test_parse_mount_point_valid_input_propagation_mode_rslave": {
 			inputString: "/tmp:/tmp:rslave",
 			expectedMount: &types.MountPoint{
 				Source:          "/tmp",
@@ -179,22 +188,30 @@ func TestParseMountPoints(t *testing.T) {
 		},
 	}
 
+	index := 0
 	inputStrings := make([]string, len(testCases))
 	expectedMounts := make([]types.MountPoint, len(testCases))
-	for index, testCase := range testCases {
-		t.Log(testCase.inputString)
 
-		res, err := ParseMountPoint(testCase.inputString)
-		testutil.AssertNil(t, err)
-		testutil.AssertEqual(t, testCase.expectedMount, res)
+	for testName, testCase := range testCases {
+		t.Run(testName, func(t *testing.T) {
 
-		inputStrings[index] = testCase.inputString
-		expectedMounts[index] = *res
+			t.Log(testCase.inputString)
+
+			res, err := ParseMountPoint(testCase.inputString)
+			testutil.AssertNil(t, err)
+			testutil.AssertEqual(t, testCase.expectedMount, res)
+
+			inputStrings[index] = testCase.inputString
+			expectedMounts[index] = *res
+			index++
+		})
 	}
 
-	res, err := ParseMountPoints(inputStrings)
-	testutil.AssertNil(t, err)
-	testutil.AssertEqual(t, expectedMounts, res)
+	t.Run("test_parse_mount_points_multiple", func(t *testing.T) {
+		res, err := ParseMountPoints(inputStrings)
+		testutil.AssertNil(t, err)
+		testutil.AssertEqual(t, expectedMounts, res)
+	})
 }
 
 func TestParseMountPointsError(t *testing.T) {
@@ -228,18 +245,18 @@ func TestParseMountPointsError(t *testing.T) {
 }
 
 func TestParsePortMappings(t *testing.T) {
-	testCases := []struct {
+	testCases := map[string]struct {
 		inputString  string
 		expectedPort *types.PortMapping
 	}{
-		{
+		"test_parse_port_mapping_input_host_and_container_port_only": {
 			inputString: "80:80",
 			expectedPort: &types.PortMapping{
 				ContainerPort: 80,
 				HostPort:      80,
 			},
 		},
-		{
+		"test_parse_port_mapping_input_host_and_container_port_plus_protocol": {
 			inputString: "88:8888/udp",
 			expectedPort: &types.PortMapping{
 				Proto:         "udp",
@@ -247,7 +264,7 @@ func TestParsePortMappings(t *testing.T) {
 				HostPort:      88,
 			},
 		},
-		{
+		"test_parse_port_mapping_input_host_range": {
 			inputString: "5000-6000:8080/udp",
 			expectedPort: &types.PortMapping{
 				Proto:         "udp",
@@ -256,7 +273,7 @@ func TestParsePortMappings(t *testing.T) {
 				HostPortEnd:   6000,
 			},
 		},
-		{
+		"test_parse_port_mapping_input_host_ip_included": {
 			inputString: "192.168.0.1:7000-8000:8081/tcp",
 			expectedPort: &types.PortMapping{
 				Proto:         "tcp",
@@ -268,22 +285,29 @@ func TestParsePortMappings(t *testing.T) {
 		},
 	}
 
+	index := 0
 	inputStrings := make([]string, len(testCases))
 	expectedPorts := make([]types.PortMapping, len(testCases))
-	for index, testCase := range testCases {
-		t.Log(testCase.inputString)
 
-		res, err := ParsePortMapping(testCase.inputString)
-		testutil.AssertNil(t, err)
-		testutil.AssertEqual(t, testCase.expectedPort, res)
+	for testName, testCase := range testCases {
+		t.Run(testName, func(t *testing.T) {
+			t.Log(testCase.inputString)
 
-		inputStrings[index] = testCase.inputString
-		expectedPorts[index] = *res
+			res, err := ParsePortMapping(testCase.inputString)
+			testutil.AssertNil(t, err)
+			testutil.AssertEqual(t, testCase.expectedPort, res)
+
+			inputStrings[index] = testCase.inputString
+			expectedPorts[index] = *res
+			index++
+		})
 	}
 
-	res, err := ParsePortMappings(inputStrings)
-	testutil.AssertNil(t, err)
-	testutil.AssertEqual(t, expectedPorts, res)
+	t.Run("test_parse_port_mappings_multiple", func(t *testing.T) {
+		res, err := ParsePortMappings(inputStrings)
+		testutil.AssertNil(t, err)
+		testutil.AssertEqual(t, expectedPorts, res)
+	})
 }
 
 func TestParsePortMappingsError(t *testing.T) {
@@ -322,12 +346,14 @@ func TestParsePortMappingsError(t *testing.T) {
 	inputStrings[0] = "80:80"
 
 	for testName, testCase := range testCases {
-		t.Log(testName)
+		t.Run(testName, func(t *testing.T) {
+			t.Log(testName)
 
-		inputStrings[1] = testCase.inputString
+			inputStrings[1] = testCase.inputString
 
-		res, err := ParsePortMappings(inputStrings)
-		testutil.AssertError(t, log.NewErrorf(testCase.errMessage+" %s", testCase.inputString), err)
-		testutil.AssertNil(t, res)
+			res, err := ParsePortMappings(inputStrings)
+			testutil.AssertError(t, log.NewErrorf(testCase.errMessage+" %s", testCase.inputString), err)
+			testutil.AssertNil(t, res)
+		})
 	}
 }
